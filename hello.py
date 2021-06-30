c2b281a6436facc7fd82370efa318d353392af7e
dinhnq <dinhbkhanoi@gmail.com>
2021-06-30 10:37:13 +0700

    add divide function to hello.py

--- hello.py
+++ hello.py
@@ -5,5 +5,11 @@ def sum( a, b):
 
 def subtract( a, b):
     return a - b
+
+def divide ( a, b):
+    if b==0:
+        return None
+    else:
+        return a/b        
 #test sum
 print ("10+5=" , sum( 10,5))
